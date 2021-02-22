f729094954e9c88218827adafa8ed6c4a7ff1b61
Jérôme Gardou <jerome.gardou@reactos.org>
2021-02-22 10:34:46 +0100

    [NTOS:MM] Fix infinite loop

--- ntoskrnl/mm/ARM3/section.c
+++ ntoskrnl/mm/ARM3/section.c
@@ -1524,6 +1524,7 @@ MiMapViewOfDataSection(IN PCONTROL_AREA ControlArea,
                     MI_WRITE_INVALID_PTE(PointerPte, TempPte);
                     RtlSetBit(&CommitBitmap, PointerPte - Vad->FirstPrototypePte);
                 }
+                PointerPte++;
             }
         }
         else
