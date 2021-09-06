dc97e23262a29a1566607577582b95912419164c
DarkFire01 <justinmiller100@gmail.com>
2021-09-06 12:51:00 -0700

    [HALX86] Implement and support MADT parse logic

--- hal/halx86/smp/mps/mps.c
+++ hal/halx86/smp/mps/mps.c
@@ -6,6 +6,11 @@
 #include <debug.h>
 PHYSICAL_ADDRESS HalpLowStubPhysicalAddress;
 PVOID HalpLowStub;
+#define TEMP_CPUMAX 32
+UINT32 PhysicalProcessorCount = 0;
+PROCESSOR_IDENTITY HalpStaticProcessorIdentity[TEMP_CPUMAX] = {{0}};
+HALP_APIC_INFO_TABLE HalpApicInfoTable;
+PPROCESSOR_IDENTITY HalpProcessorIdentity = NULL;
 
 VOID
 HalpInitializeInterruptLoc(IN PLOADER_PARAMETER_BLOCK LoaderBlock)
@@ -29,7 +34,23 @@ HalpInitializeInterruptLoc(IN PLOADER_PARAMETER_BLOCK LoaderBlock)
 }
 
 VOID
-HalpParseApicTables()
+HalpParseApicTables(IN PLOADER_PARAMETER_BLOCK LoaderBlock)
 {
 
+}
+
+VOID
+HalpPrintApicTables()
+{ 
+    DPRINT1("HAL has detected a physical processor count of: %d\n", PhysicalProcessorCount);
+
+    for(UINT32 i = 0; i < PhysicalProcessorCount; i++)
+    {
+        DPRINT1("Information about the following processor is for processors number: %d\n", i);
+        DPRINT1("   The BSPCheck is set to: %X\n", HalpStaticProcessorIdentity[i].BSPCheck);
+        DPRINT1("   The LapicID is set to: %X\n", HalpStaticProcessorIdentity[i].LapicId);
+        DPRINT1("   The ProcessorID is set to: %X\n", HalpStaticProcessorIdentity[i].ProcessorId);
+        DPRINT1("   The ProcessorStated check is set to: %X\n", HalpStaticProcessorIdentity[i].ProcessorStarted);
+        DPRINT1("   The ROSProcessorNumber is set to: %X\n", HalpStaticProcessorIdentity[i].ROSProcessorNumber);
+    }
 }
