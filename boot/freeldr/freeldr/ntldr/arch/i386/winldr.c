7dd016550d433f90e6c641c52f9580cccee9a2ca
Vadim Galyant <vgal@rambler.ru>
2022-01-16 13:28:10 +0400

    [FREELDR] Small corrections.

--- boot/freeldr/freeldr/ntldr/arch/i386/winldr.c
+++ boot/freeldr/freeldr/ntldr/arch/i386/winldr.c
@@ -392,7 +392,8 @@ MempSetupPaging(IN PFN_NUMBER StartPage,
 
             if (KernelMapping)
             {
-                if (KernelPde[Page & PAE_PDE_MASK].Valid) WARN("KernelPde already mapped\n");
+                if (KernelPde[Page & PAE_PDE_MASK].Valid)
+                    WARN("KernelPde already mapped\n");
 
                 KernelPde[Page & PAE_PDE_MASK].PageFrameNumber = Page;
                 KernelPde[Page & PAE_PDE_MASK].Valid = (Page != 0);
@@ -640,7 +641,7 @@ WinLdrSetProcessorContext(
         __writecr3((ULONG_PTR)PaeTables);
 
         /* Enable PAE */
-        __writecr4(__readcr4() | 0x20);
+        __writecr4(__readcr4() | CR4_PAE);
     }
 
     /* Enable paging */
