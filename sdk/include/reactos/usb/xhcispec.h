1623b78939c772b389e23225acdb00089d66de9b
Justin Miller <justinmiller100@gmail.com>
2022-01-16 02:23:02 -0800

    [SDK] Add more TRBs

--- sdk/include/reactos/usb/xhcispec.h
+++ sdk/include/reactos/usb/xhcispec.h
@@ -117,35 +117,34 @@ typedef struct _XHCI_GENERIC_TRB {
 }XHCI_GENERIC_TRB, *PXHCI_GENERIC_TRB;
 C_ASSERT(sizeof(XHCI_GENERIC_TRB) == 16);
 
-
-
-/* Enable Slot TRB ********************************************************************************/
-
-/* 6.4.3.2 */
-typedef struct _XHCI_ENABLE_SLOT_COMMAND_TRB
+typedef struct _XHCI_EVENT_TRANSFER_TRB
 {
-    struct 
+    struct
     {
-        ULONG RsvdZ1                         : 32;
+        ULONG TrbPtrLow                     : 32;
     };
-    struct 
+    struct
     {
-        ULONG RsvdZ2                         : 32;
+        ULONG TrbPtrHigh                    : 32;
     };
-    struct 
+    struct
     {
-        ULONG RsvdZ3                         : 32;
+        ULONG CompletionCode                : 23;
+        ULONG TrbTransferLen                : 9;
     };
-    struct 
+    struct
     {
-        ULONG CycleBit                       : 1;
-        ULONG RsvdZ4                         : 10;
-        ULONG TRBType                        : 6;
-        ULONG SlotType                       : 5;
-        ULONG RsvdZ5                         : 10;
+        ULONG CycleBit                      : 1;
+        ULONG RsvdZ1                        : 1;
+        ULONG EventData                     : 1;
+        ULONG RsvdZ2                        : 7;
+        ULONG TRBType                       : 11;
+        ULONG EndpointID                    : 5;
+        ULONG RsvdZ3                        : 3;
+        ULONG SlotID                        : 8;
     };
-} XHCI_ENABLE_SLOT_COMMAND_TRB, *PXHCI_ENABLE_SLOT_COMMAND_TRB;
-C_ASSERT(sizeof(XHCI_ENABLE_SLOT_COMMAND_TRB) == 16);
+    
+} XHCI_EVENT_TRANSFER_TRB, *PXHCI_EVENT_TRANSFER_TRB ;
 
 /* Configure Endpoint Command TRB *****************************************************************/
 
@@ -213,45 +212,6 @@ typedef struct _XHCI_LINK_TRB
     };
 } XHCI_LINK_TRB;
 C_ASSERT(sizeof(XHCI_LINK_TRB) == 16);
-
-/* Command TRB ************************************************************************************/
-
-/* 4.11.4 */
-typedef struct _XHCI_COMMAND_NO_OP_TRB 
-{
-    ULONG RsvdZ1;
-    ULONG RsvdZ2;
-    ULONG RsvdZ3;
-    struct
-    {
-        ULONG CycleBit                       : 1;
-        ULONG RsvdZ4                         : 9;
-        ULONG TRBType                        : 6;
-        ULONG RsvdZ5                         : 16;
-    };
-} XHCI_COMMAND_NO_OP_TRB;
-C_ASSERT(sizeof(XHCI_COMMAND_NO_OP_TRB) == 16);
-
-/* Stop Endpoint Commands TRB ************************************************************************************/
-
-/* 6.4.3.8 */
-typedef struct _XHCI_STOP_ENDPOINT_COMMAND_TRB
-{
-    ULONG RsvdZ1;
-    ULONG RsvdZ2;
-    ULONG RsvdZ3;
-    struct
-    {
-        ULONG CycleBit                       : 1;
-        ULONG RsvdZ4                         : 9;
-        ULONG TRBType                        : 6;
-        ULONG EndpointID                     : 5;
-        ULONG RsvdZ5                         : 2;
-        ULONG Suspend                        : 1;
-        ULONG SlotID                         : 8;
-    };
-} XHCI_STOP_ENDPOINT_COMMAND_TRB, *PXHCI_STOP_ENDPOINT_COMMAND_TRB;
-C_ASSERT(sizeof(XHCI_STOP_ENDPOINT_COMMAND_TRB) == 16);
 
 typedef union _XHCI_COMMAND_TRB 
 {
@@ -330,6 +290,42 @@ typedef struct _XHCI_CONTROL_DATA_TRB
 } XHCI_CONTROL_DATA_TRB;
 C_ASSERT(sizeof(XHCI_CONTROL_DATA_TRB) == 16);
 
+/* 6.4.1.3 - Isoch Control TRB */
+typedef struct _XHCI_CONTROL_DATA_TRB 
+{
+    struct 
+    {
+        ULONG DataBufferPointerLo            : 32;
+    };
+    struct 
+    {
+        ULONG DataBufferPointerHi            : 32;
+    };
+    struct 
+    {
+        ULONG TRBTransferLength              : 17;
+        ULONG TDSize                         : 5;
+        ULONG InterrupterTarget              : 10;
+    };
+    struct 
+    {
+        ULONG CycleBit                       : 1;
+        ULONG EvaluateNextTRB                : 1;
+        ULONG InterruptOnShortPacket         : 1;
+        ULONG NoSnoop                        : 1;
+        ULONG ChainBit                       : 1;
+        ULONG InterruptOnCompletion          : 1;
+        ULONG ImmediateData                  : 1;
+        ULONG TransferBurstControl           : 2;
+        ULONG BlockEventInterrupt            : 1;
+        ULONG TRBType                        : 6;
+        ULONG TLBPC                          : 4;
+        ULONG FrameID                        : 10;
+        ULONG SIA                            : 1;
+    };
+} XHCI_CONTROL_DATA_TRB;
+C_ASSERT(sizeof(XHCI_CONTROL_DATA_TRB) == 16);
+
 typedef struct _XHCI_CONTROL_STATUS_TRB 
 {
     struct 
@@ -359,6 +355,31 @@ typedef struct _XHCI_CONTROL_STATUS_TRB
 } XHCI_CONTROL_STATUS_TRB;
 C_ASSERT(sizeof(XHCI_CONTROL_STATUS_TRB) == 16);
 
+typedef struct _XHCI_BANDWITH_REQUEST_TRB 
+{
+    struct 
+    {
+        ULONG RsvdZ1                         : 32;
+    };
+    struct 
+    {
+        ULONG RsvdZ2                         : 32;
+    };
+    struct 
+    {
+        ULONG RsvdZ3                         : 24;
+        ULONG CompletetionCode               : 8;
+    };
+    struct 
+    {
+        ULONG CycleBit                       : 1;
+        ULONG RsvdZ4                         : 10;
+        ULONG TRBType                        : 6;
+        ULONG RsvdZ5                         : 8;
+        ULONG SlotID                         : 8;
+    };
+} XHCI_BANDWITH_REQUEST_TRB;
+C_ASSERT(sizeof(XHCI_BANDWITH_REQUEST_TRB) == 16);
 typedef union _XHCI_CONTROL_TRB 
 {
     XHCI_CONTROL_SETUP_TRB  SetupTRB;
@@ -368,74 +389,13 @@ typedef union _XHCI_CONTROL_TRB
 } XHCI_CONTROL_TRB, *PXHCI_CONTROL_TRB;  
 C_ASSERT(sizeof(XHCI_CONTROL_TRB) == 16);
 
-typedef struct _XHCI_EVENT_COMMAND_COMPLETION_TRB
-{
-    struct 
-    {
-        ULONG RsvdZ1                         : 4;
-        ULONG CommandTRBPointerLo            : 28;
-    };
-    ULONG CommandTRBPointerHi;
-    struct 
-    {
-        ULONG CommandCompletionParam         : 24;
-        ULONG CompletionCode                 : 8;
-    };
-    struct 
-    {
-        ULONG CycleBit                       : 1;
-        ULONG RsvdZ2                         : 9;
-        ULONG TRBType                        : 6;
-        ULONG VFID                           : 8;
-        ULONG SlotID                         : 8;
-    };
-} XHCI_EVENT_COMMAND_COMPLETION_TRB;
-C_ASSERT(sizeof(XHCI_EVENT_COMMAND_COMPLETION_TRB) == 16);
-
-typedef struct _XHCI_EVENT_PORT_STATUS_CHANGE_TRB
-{
-    struct 
-    {
-        ULONG RsvdZ1                         : 24;
-        ULONG PortID                         : 8;
-    };
-    ULONG RsvdZ2;
-    struct 
-    {
-        ULONG RsvdZ3                         : 24;
-        ULONG CompletionCode                 : 8;
-    };
-    struct 
-    {
-        ULONG CycleBit                       : 1;
-        ULONG RsvdZ4                         : 9;
-        ULONG TRBType                        : 6;
-        ULONG RsvdZ5                         : 16;
-    };
-} XHCI_EVENT_PORT_STATUS_CHANGE_TRB;
-C_ASSERT(sizeof(XHCI_EVENT_PORT_STATUS_CHANGE_TRB) == 16);
-
-typedef struct _XHCI_EVENT_GENERIC_TRB
-{
-    ULONG Word0;
-    ULONG Word1;
-    ULONG Word2;
-    struct 
-    {
-        ULONG CycleBit                       : 1;
-        ULONG RsvdZ1                         : 9;
-        ULONG TRBType                        : 6;
-        ULONG RsvdZ2                         : 8;
-        ULONG SlotID                         : 8;
-    };
-}XHCI_EVENT_GENERIC_TRB;
-C_ASSERT(sizeof(XHCI_EVENT_GENERIC_TRB) == 16);
-
 typedef union _XHCI_EVENT_TRB 
 {
     XHCI_EVENT_COMMAND_COMPLETION_TRB   CommandCompletionTRB;
     XHCI_EVENT_PORT_STATUS_CHANGE_TRB   PortStatusChangeTRB;
     XHCI_EVENT_GENERIC_TRB              EventGenericTRB;
+    XHCI_EVENT_TRANSFER_TRB             EventTransferTRB;
+    XHCI_BANDWITH_REQUEST_TRB           BandwithRequestTRB;
 }XHCI_EVENT_TRB, *PXHCI_EVENT_TRB;
 C_ASSERT(sizeof(XHCI_EVENT_TRB) == 16);
 
@@ -451,17 +411,6 @@ typedef union _XHCI_LINK_ADDR
     };
     ULONGLONG AsULONGLONG;
 } XHCI_LINK_ADDR;
-
-// 6.5
-typedef struct _XHCI_EVENT_RING_SEGMENT_TABLE
-{
-    ULONGLONG RingSegmentBaseAddr;
-    struct 
-    {
-        ULONGLONG RingSegmentSize            :  16;
-        ULONGLONG RsvdZ                      :  48;
-    };
-} XHCI_EVENT_RING_SEGMENT_TABLE;
 
 typedef union _XHCI_TRB 
 {
--- sdk/include/reactos/usb/xhcitrb.h
+++ sdk/include/reactos/usb/xhcitrb.h
@@ -7,9 +7,182 @@
 #include <usbbusif.h>
 #include <drivers/usbport/usbmport.h>
 
-/* Address Device TRB ********************************************************************************/
+/* Transfer TRBs *************************************************************************************/
 
-/* 6.4.3.4 */
+/* 6.4.1.1 - Normal Transfer TRB */
+typedef struct _XHCI_NORMAL_TRANSFER_TRB
+{
+    struct
+    {
+        ULONG DataBufPtrLow                  : 32;
+    }
+    struct
+    {
+        ULONG DataBufPtrHigh                 : 32;
+    }
+    struct
+    {
+        ULONG TrbTransferLen                 : 17;
+        ULONG TDSize                         : 5;
+        ULONG InterruptTarget;               : 10;
+    }
+    struct 
+    {
+        ULONG CycleBit                       : 1;
+        ULONG ENT                            : 1;
+        ULONG ISP                            : 1;
+        ULONG NS                             : 1;
+        ULONG CH                             : 1;
+        ULONG IOC                            : 1;
+        ULONG IDT                            : 1;
+        ULONG RsvdZ1                         : 2;
+        ULONG BEI                            : 1;
+        ULONG TRBType                        : 6;
+        ULONG RsvdZ1                         : 16;
+    };
+} XHCI_NORMAL_TRANSFER_TRB, *PXHCI_NORMAL_TRANSFER_TRB;
+C_ASSERT(sizeof(XHCI_EVENT_GENERIC_TRB) == 16);
+
+/* Event TRBs ****************************************************************************************/
+
+/* 6.5 - Event Ring segment table */
+typedef struct _XHCI_EVENT_RING_SEGMENT_TABLE
+{
+    ULONGLONG RingSegmentBaseAddr;
+    struct 
+    {
+        ULONGLONG RingSegmentSize            :  16;
+        ULONGLONG RsvdZ                      :  48;
+    };
+} XHCI_EVENT_RING_SEGMENT_TABLE;
+
+typedef struct _XHCI_EVENT_GENERIC_TRB
+{
+    ULONG Word0;
+    ULONG Word1;
+    ULONG Word2;
+    struct 
+    {
+        ULONG CycleBit                       : 1;
+        ULONG RsvdZ1                         : 9;
+        ULONG TRBType                        : 6;
+        ULONG RsvdZ2                         : 8;
+        ULONG SlotID                         : 8;
+    };
+}XHCI_EVENT_GENERIC_TRB;
+C_ASSERT(sizeof(XHCI_EVENT_GENERIC_TRB) == 16);
+
+typedef struct _XHCI_EVENT_COMMAND_COMPLETION_TRB
+{
+    struct 
+    {
+        ULONG RsvdZ1                         : 4;
+        ULONG CommandTRBPointerLo            : 28;
+    };
+    ULONG CommandTRBPointerHi;
+    struct 
+    {
+        ULONG CommandCompletionParam         : 24;
+        ULONG CompletionCode                 : 8;
+    };
+    struct 
+    {
+        ULONG CycleBit                       : 1;
+        ULONG RsvdZ2                         : 9;
+        ULONG TRBType                        : 6;
+        ULONG VFID                           : 8;
+        ULONG SlotID                         : 8;
+    };
+} XHCI_EVENT_COMMAND_COMPLETION_TRB;
+C_ASSERT(sizeof(XHCI_EVENT_COMMAND_COMPLETION_TRB) == 16);
+
+typedef struct _XHCI_EVENT_PORT_STATUS_CHANGE_TRB
+{
+    struct 
+    {
+        ULONG RsvdZ1                         : 24;
+        ULONG PortID                         : 8;
+    };
+    ULONG RsvdZ2;
+    struct 
+    {
+        ULONG RsvdZ3                         : 24;
+        ULONG CompletionCode                 : 8;
+    };
+    struct 
+    {
+        ULONG CycleBit                       : 1;
+        ULONG RsvdZ4                         : 9;
+        ULONG TRBType                        : 6;
+        ULONG RsvdZ5                         : 16;
+    };
+} XHCI_EVENT_PORT_STATUS_CHANGE_TRB;
+C_ASSERT(sizeof(XHCI_EVENT_PORT_STATUS_CHANGE_TRB) == 16);
+
+/* Command TRBs **************************************************************************************/
+
+/* 6.4.3.2 - Enable Slot TRB */
+typedef struct _XHCI_ENABLE_SLOT_COMMAND_TRB
+{
+    struct 
+    {
+        ULONG RsvdZ1                         : 32;
+    };
+    struct 
+    {
+        ULONG RsvdZ2                         : 32;
+    };
+    struct 
+    {
+        ULONG RsvdZ3                         : 32;
+    };
+    struct 
+    {
+        ULONG CycleBit                       : 1;
+        ULONG RsvdZ4                         : 10;
+        ULONG TRBType                        : 6;
+        ULONG SlotType                       : 5;
+        ULONG RsvdZ5                         : 10;
+    };
+} XHCI_ENABLE_SLOT_COMMAND_TRB, *PXHCI_ENABLE_SLOT_COMMAND_TRB;
+C_ASSERT(sizeof(XHCI_ENABLE_SLOT_COMMAND_TRB) == 16);
+
+/* 4.11.4 - No Operation command */
+typedef struct _XHCI_COMMAND_NO_OP_TRB 
+{
+    ULONG RsvdZ1;
+    ULONG RsvdZ2;
+    ULONG RsvdZ3;
+    struct
+    {
+        ULONG CycleBit                       : 1;
+        ULONG RsvdZ4                         : 9;
+        ULONG TRBType                        : 6;
+        ULONG RsvdZ5                         : 16;
+    };
+} XHCI_COMMAND_NO_OP_TRB;
+C_ASSERT(sizeof(XHCI_COMMAND_NO_OP_TRB) == 16);
+
+/* 6.4.3.8 - Stop Endpoint Commands TRB  */
+typedef struct _XHCI_STOP_ENDPOINT_COMMAND_TRB
+{
+    ULONG RsvdZ1;
+    ULONG RsvdZ2;
+    ULONG RsvdZ3;
+    struct
+    {
+        ULONG CycleBit                       : 1;
+        ULONG RsvdZ4                         : 9;
+        ULONG TRBType                        : 6;
+        ULONG EndpointID                     : 5;
+        ULONG RsvdZ5                         : 2;
+        ULONG Suspend                        : 1;
+        ULONG SlotID                         : 8;
+    };
+} XHCI_STOP_ENDPOINT_COMMAND_TRB, *PXHCI_STOP_ENDPOINT_COMMAND_TRB;
+C_ASSERT(sizeof(XHCI_STOP_ENDPOINT_COMMAND_TRB) == 16);
+
+/* 6.4.3.4 - Address Device TRB */
 typedef struct _XHCI_ADDRESS_DEVICE_COMMAND_TRB
 {
     /* 00h - 03h */
@@ -40,3 +213,5 @@ typedef struct _XHCI_ADDRESS_DEVICE_COMMAND_TRB
     };
 } XHCI_ADDRESS_DEVICE_COMMAND_TRB, *PXHCI_ADDRESS_DEVICE_COMMAND_TRB;
 C_ASSERT(sizeof(XHCI_ADDRESS_DEVICE_COMMAND_TRB) == 16);
+
+/* Other TRBs ****************************************************************************************/
