2b6cce4dc74eb6c3fea2566500b3a296cd1fede2
DarkFire01 <Justinmiller100@gmail.com>
2022-05-07 17:27:36 -0700

    [HALARM] Fix arm64 build

---
+++ hal/halarm/apple/halinit_up.c
@@ -0,0 +1,31 @@
+
+/*
+ * PROJECT:         ReactOS HAL
+ * LICENSE:         BSD - See COPYING.ARM in the top level directory
+ * PURPOSE:         Versatile Board-Specific HAL Initialization
+ * COPYRIGHT:
+ */
+
+/* INCLUDES *******************************************************************/
+
+#include <hal.h>
+#define NDEBUG
+#include <debug.h>
+
+/* GLOBALS ********************************************************************/
+
+/* PRIVATE FUNCTIONS **********************************************************/
+
+VOID
+HalpInitPhase0(IN PLOADER_PARAMETER_BLOCK LoaderBlock)
+{
+
+}
+
+VOID
+HalpInitPhase1(VOID)
+{
+
+}
+
+/* EOF */
