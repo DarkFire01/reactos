068ff8001d53b3c9fd0101d26eed7b7521f00c3d
Justin Miller <Justinmiller100@gmail.com>
2023-03-20 08:03:58 -0700

    [UEFI] Add keyboard input handler

--- boot/freeldr/freeldr/arch/uefi/stubs.c
+++ boot/freeldr/freeldr/arch/uefi/stubs.c
@@ -60,18 +60,6 @@ UefiPcBeep(VOID)
     /* Not possible on UEFI, for now */
 }
 
-BOOLEAN
-UefiConsKbHit(VOID)
-{
-    return FALSE;
-}
-
-int
-UefiConsGetCh(void)
-{
-    return 0;
-}
-
 VOID
 UefiHwIdle(VOID)
 {
