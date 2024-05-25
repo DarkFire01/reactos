7b0ab547e630cbc753be521a1c24c602d9826b68
winesync <ros-dev@reactos.org>
2024-05-26 00:30:39 +0200

    [WINESYNC] riched20: Implement TxDraw().
    Signed-off-by: Huw Davies <huw@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id 02f083641ec1aee19d8c2e2b410619cdd5e5f45d by Huw Davies <huw@codeweavers.com>

--- sdk/include/psdk/textserv.h
+++ sdk/include/psdk/textserv.h
@@ -108,7 +108,7 @@ DECLARE_INTERFACE_(ITextServices,IUnknown)
         INT y,
         DWORD* pHitResult) PURE;
 
-    THISCALLMETHOD_(HRESULT,OnTxInplaceActivate)( THIS_
+    THISCALLMETHOD_(HRESULT,OnTxInPlaceActivate)( THIS_
         LPCRECT prcClient) PURE;
 
     THISCALLMETHOD_(HRESULT,OnTxInplaceDeactivate)( THIS ) PURE;
