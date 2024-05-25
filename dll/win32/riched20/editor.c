47cb0a096930ab42ccbc19a1fc27b5a7dca0108c
winesync <ros-dev@reactos.org>
2024-05-25 13:14:39 +0200

    [WINESYNC] riched20: Pass and return paragraph ptrs in the table append row function.
    Signed-off-by: Huw Davies <huw@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id f0783863f90543580a47766e7235b1ddc8e272fb by Huw Davies <huw@codeweavers.com>

--- dll/win32/riched20/editor.c
+++ dll/win32/riched20/editor.c
@@ -2512,7 +2512,7 @@ static BOOL handle_enter(ME_TextEditor *editor)
                 if (para->member.para.nFlags & MEPF_ROWEND)
                 {
                     /* Add a new table row after this row. */
-                    para = ME_AppendTableRow(editor, para);
+                    para = para_get_di( table_append_row( editor, &para->member.para ) );
                     para = para->member.para.next_para;
                     editor->pCursors[0].pPara = para;
                     editor->pCursors[0].pRun = ME_FindItemFwd(para, diRun);
@@ -2560,7 +2560,7 @@ static BOOL handle_enter(ME_TextEditor *editor)
                         if (from == to)
                         {
                             ME_ContinueCoalescingTransaction(editor);
-                            para = ME_AppendTableRow(editor, para);
+                            para = para_get_di( table_append_row( editor, &para->member.para ) );
                             editor->pCursors[0].pPara = para;
                             editor->pCursors[0].pRun = ME_FindItemFwd(para, diRun);
                             editor->pCursors[0].nOffset = 0;
@@ -2591,7 +2591,7 @@ static BOOL handle_enter(ME_TextEditor *editor)
                         else
                         {
                             editor->pCursors[1] = editor->pCursors[0];
-                            para = ME_AppendTableRow(editor, para);
+                            para = para_get_di( table_append_row( editor, &para->member.para ) );
                             editor->pCursors[0].pPara = para;
                             editor->pCursors[0].pRun = ME_FindItemFwd(para, diRun);
                             editor->pCursors[0].nOffset = 0;
