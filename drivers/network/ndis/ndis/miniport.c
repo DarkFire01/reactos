c34509788eb70a2781176a836ef45888c19ad080
Justin Miller <justin.miller@reactos.org>
2025-11-16 08:18:16 -0800

    [CODEREVIEW]
    Co-authored-by: Hermès BÉLUSCA - MAÏTO <hermes.belusca-maito@reactos.org>

--- drivers/network/ndis/ndis/miniport.c
+++ drivers/network/ndis/ndis/miniport.c
@@ -2622,7 +2622,6 @@ NdisIAddDevice(
   KeInitializeSpinLock(&Adapter->NdisMiniportBlock.Lock);
   InitializeListHead(&Adapter->ProtocolListHead);
 
-
   Status = IoRegisterDeviceInterface(PhysicalDeviceObject,
                                      &GUID_DEVINTERFACE_NET,
                                      NULL,
