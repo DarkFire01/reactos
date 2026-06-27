480aea9bc452c35bd8e450ec03c1fcd104fece69
Justin Miller <justin.miller@reactos.org>
2026-06-26 20:27:54 -0700

    more tests

--- modules/rostests/apitests/alpc/testlist.c
+++ modules/rostests/apitests/alpc/testlist.c
@@ -20,6 +20,22 @@ extern void func_NtAlpcSecurityContext(void);
 extern void func_CompletionList(void);
 extern void func_ViewTransfer(void);
 extern void func_NtAlpcConnectPortEx(void);
+extern void func_ConnectPending(void);
+extern void func_CommPortReceive(void);
+extern void func_SectionRounding(void);
+extern void func_OpenSenderDatagram(void);
+extern void func_HandleTransfer(void);
+extern void func_ImpersonateDenied(void);
+extern void func_CancelRoundTrip(void);
+extern void func_DisconnectPeer(void);
+extern void func_ContextAttribute(void);
+extern void func_TokenAttribute(void);
+extern void func_SecurityAttribute(void);
+extern void func_CompletionPortAssoc(void);
+extern void func_LargeMessage(void);
+extern void func_ReserveEnforcement(void);
+extern void func_CompletionListDelivery(void);
+extern void func_LegacyLpc(void);
 
 const struct test winetest_testlist[] =
 {
@@ -42,5 +58,21 @@ const struct test winetest_testlist[] =
     { "CompletionList",          func_CompletionList },
     { "ViewTransfer",            func_ViewTransfer },
     { "NtAlpcConnectPortEx",     func_NtAlpcConnectPortEx },
+    { "ConnectPending",          func_ConnectPending },
+    { "CommPortReceive",         func_CommPortReceive },
+    { "SectionRounding",         func_SectionRounding },
+    { "OpenSenderDatagram",      func_OpenSenderDatagram },
+    { "HandleTransfer",          func_HandleTransfer },
+    { "ImpersonateDenied",       func_ImpersonateDenied },
+    { "CancelRoundTrip",         func_CancelRoundTrip },
+    { "DisconnectPeer",          func_DisconnectPeer },
+    { "ContextAttribute",        func_ContextAttribute },
+    { "TokenAttribute",          func_TokenAttribute },
+    { "SecurityAttribute",       func_SecurityAttribute },
+    { "CompletionPortAssoc",     func_CompletionPortAssoc },
+    { "LargeMessage",            func_LargeMessage },
+    { "ReserveEnforcement",      func_ReserveEnforcement },
+    { "CompletionListDelivery",  func_CompletionListDelivery },
+    { "LegacyLpc",               func_LegacyLpc },
     { 0, 0 }
 };
