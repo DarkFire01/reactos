f6c3aa8ac43a27ca83c325325425a98f56b1762d
Justin Miller <justin.miller@reactos.org>
2026-08-31 19:44:56 -0700

    [PCIX] Read the command word into the variable that is then used

--- drivers/bus/pcix/utils.c
+++ drivers/bus/pcix/utils.c
@@ -1292,7 +1292,7 @@ PciDecodeEnable(IN PPCI_PDO_EXTENSION PdoExtension,
         {
             /* Otherwise, read the current command */
             PciReadDeviceConfig(PdoExtension,
-                                &Command,
+                                &CommandValue,
                                 FIELD_OFFSET(PCI_COMMON_HEADER, Command),
                                 sizeof(USHORT));
         }
