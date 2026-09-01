03a3bf4bc146867cf89dbdf154c4a39743e339e0
Justin Miller <justin.miller@reactos.org>
2026-08-31 21:45:10 -0700

    [PCIX] Drop the unimplemented marker from a path that is implemented

--- drivers/bus/pcix/enum.c
+++ drivers/bus/pcix/enum.c
@@ -2195,9 +2195,7 @@ PciScanBus(IN PPCI_FDO_EXTENSION DeviceExtension)
             Status = PciGetBiosConfig(NewExtension, BiosData);
             if (NT_SUCCESS(Status))
             {
-                /* This path has not yet been fully tested by eVb */
                 DPRINT1("Have BIOS configuration!\n");
-                UNIMPLEMENTED;
 
                 /* Check if the PCI BIOS configuration has changed */
                 if (!PcipIsSameDevice(NewExtension, BiosData))
