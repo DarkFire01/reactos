6a524315228181b0616a5c41787c13203952c197
Justin Miller <justin.miller@reactos.org>
2026-08-31 23:37:07 -0700

    [PCIX] Find the $PIR table where the loader actually put it

--- sdk/include/reactos/drivers/pci/pci.h
+++ sdk/include/reactos/drivers/pci/pci.h
@@ -85,6 +85,9 @@ typedef struct _SLOT_INFO
     UCHAR Reserved;
 } SLOT_INFO, *PSLOT_INFO;
 
+#define PCI_IRQ_ROUTING_TABLE_SIGNATURE 'RIP$'
+#define PCI_IRQ_ROUTING_TABLE_VERSION   0x0100
+
 typedef struct _PCI_IRQ_ROUTING_TABLE
 {
     ULONG Signature;
