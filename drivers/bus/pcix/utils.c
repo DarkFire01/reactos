e2ac6e465ea2fd97de3d4e71228ca7b99aad343d
Justin Miller <justin.miller@reactos.org>
2026-08-31 23:38:56 -0700

    [PCIX] Trace the slot number lookup instead of printing it

--- drivers/bus/pcix/utils.c
+++ drivers/bus/pcix/utils.c
@@ -1348,23 +1348,23 @@ PciDetermineSlotNumber(IN PPCI_PDO_EXTENSION PdoExtension,
 
     /* Check if a $PIR from the BIOS is used (legacy IRQ routing) */
     ParentExtension = PdoExtension->ParentFdoExtension;
-    DPRINT1("Slot lookup for %d.%u.%u\n",
-            ParentExtension ? ParentExtension->BaseBus : -1,
-            PdoExtension->Slot.u.bits.DeviceNumber,
-            PdoExtension->Slot.u.bits.FunctionNumber);
+    DPRINT("Slot lookup for %d.%u.%u\n",
+           ParentExtension ? ParentExtension->BaseBus : -1,
+           PdoExtension->Slot.u.bits.DeviceNumber,
+           PdoExtension->Slot.u.bits.FunctionNumber);
     if ((PciIrqRoutingTable) && (ParentExtension))
     {
         /* Read every slot information entry */
         SlotInfo = &PciIrqRoutingTable->Slot[0];
-        DPRINT1("$PIR %p is %lx bytes, slot 0 is at: %p\n",
-                PciIrqRoutingTable, PciIrqRoutingTable->TableSize, SlotInfo);
+        DPRINT("$PIR %p is %u bytes, slot 0 is at: %p\n",
+               PciIrqRoutingTable, PciIrqRoutingTable->TableSize, SlotInfo);
         while (SlotInfo < (PSLOT_INFO)((ULONG_PTR)PciIrqRoutingTable +
                                        PciIrqRoutingTable->TableSize))
         {
-            DPRINT1("Slot Info: %u.%u->#%u\n",
-                    SlotInfo->BusNumber,
-                    SlotInfo->DeviceNumber,
-                    SlotInfo->SlotNumber);
+            DPRINT("Slot Info: %u.%u->#%u\n",
+                   SlotInfo->BusNumber,
+                   SlotInfo->DeviceNumber,
+                   SlotInfo->SlotNumber);
 
             /* Check if this slot information matches the PDO being queried */
             if ((ParentExtension->BaseBus == SlotInfo->BusNumber) &&
