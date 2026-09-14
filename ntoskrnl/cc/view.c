c69f4b3a758af5da5e79a948415252e02f34386e
Justin Miller <justin.miller@reactos.org>
2026-09-13 19:43:15 -0700

    [NTOS:MM][NTOS:CC] Trim unused cache views from the working set manager instead of the legacy balancer

--- ntoskrnl/cc/view.c
+++ ntoskrnl/cc/view.c
@@ -39,8 +39,9 @@
 
 /* GLOBALS *******************************************************************/
 
-LIST_ENTRY DirtyVacbListHead;
-static LIST_ENTRY VacbLruListHead;
+/* Empty from the start, the working set manager may trim the cache before it is initialized */
+LIST_ENTRY DirtyVacbListHead = { &DirtyVacbListHead, &DirtyVacbListHead };
+static LIST_ENTRY VacbLruListHead = { &VacbLruListHead, &VacbLruListHead };
 
 NPAGED_LOOKASIDE_LIST iBcbLookasideList;
 static NPAGED_LOOKASIDE_LIST SharedCacheMapLookasideList;
