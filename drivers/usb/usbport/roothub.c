18bf479d133da1c1e6a85e27045b93eb7a50f1c9
Justin Miller <justin.miller@reactos.org>
2026-09-13 17:29:04 -0700

    [USBPORT] Use the companion miniport when powering companion ports
    The EHCI root hub called its own miniport routines with each companion controller's extension.

--- drivers/usb/usbport/roothub.c
+++ drivers/usb/usbport/roothub.c
@@ -1006,9 +1006,10 @@ USBPORT_RootHubPowerAndChirpAllCcPorts(IN PDEVICE_OBJECT FdoDevice)
              NumController < CompanionControllersList->Count;
              NumController++)
         {
-            CompanionPacket = &FdoExtension->MiniPortInterface->Packet;
-
             CompanionFdoExtension = (*Entry)->DeviceExtension;
+
+            /* The companion extension goes with the companion miniport */
+            CompanionPacket = &CompanionFdoExtension->MiniPortInterface->Packet;
 
             CompanionPacket->RH_GetRootHubData(CompanionFdoExtension->MiniPortExt,
                                                &RootHubData);
