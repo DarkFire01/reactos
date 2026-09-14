ddc342e7cc0b015b3eb90597b549246dd6b9d46b
Justin Miller <justin.miller@reactos.org>
2026-09-13 18:45:45 -0700

    [NTOS:MM] Keep shared pages in process working sets and trim them under pressure

--- ntoskrnl/include/internal/mm.h
+++ ntoskrnl/include/internal/mm.h
@@ -1839,8 +1839,15 @@ MiRemoveFromWorkingSetList(
 
 VOID
 NTAPI
-MiAddPrivatePageToWorkingSet(
-    _In_ PVOID Address);
+MiAddValidPageToWorkingSet(
+    _In_ PVOID Address,
+    _In_ ULONG Protection);
+
+VOID
+NTAPI
+MiRemoveSharedPageFromWorkingSet(
+    _In_ PVOID Address,
+    _In_ PMMPFN Pfn);
 
 _Requires_exclusive_lock_held_(WorkingSet->WorkingSetMutex)
 VOID
