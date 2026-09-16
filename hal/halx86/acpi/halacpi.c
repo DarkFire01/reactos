014ffedcca9bf994540d6cac5c03d9ab2a88d4ff
Justin Miller <justin.miller@reactos.org>
2026-09-15 18:22:22 -0700

    [HALACPI] Grant the device IDT entries to the ACPI driver

--- hal/halx86/acpi/halacpi.c
+++ hal/halx86/acpi/halacpi.c
@@ -1018,11 +1018,38 @@ HalpIs16BitPortDecodeSupported(VOID)
     return CM_RESOURCE_PORT_16_BIT_DECODE;
 }
 
+/* IDT entries the APIC HALs give to the ACPI driver for device interrupts */
+#define HALP_ACPI_DEVICE_VECTOR_FIRST 0x51
+#define HALP_ACPI_DEVICE_VECTOR_LAST  0xBE
+
+static
+BOOLEAN
+HalpIsAcpiDeviceVectorFree(
+    _In_ ULONG Vector)
+{
+    return !(HalpIDTUsageFlags[Vector].Flags & IDT_REGISTERED);
+}
+
 VOID
 NTAPI
 HalpAcpiDetectResourceListSize(OUT PULONG ListSize)
 {
+    ULONG Vector;
+
     PAGED_CODE();
+
+    if (HalpInterruptControllerType == HALP_INTERRUPT_CONTROLLER_APIC)
+    {
+        *ListSize = 0;
+        for (Vector = HALP_ACPI_DEVICE_VECTOR_FIRST;
+             Vector <= HALP_ACPI_DEVICE_VECTOR_LAST;
+             Vector++)
+        {
+            if (HalpIsAcpiDeviceVectorFree(Vector))
+                (*ListSize)++;
+        }
+        return;
+    }
 
     /* One element if there is a SCI */
     *ListSize = HalpFixedAcpiDescTable.sci_int_vector ? 1: 0;
@@ -1032,7 +1059,9 @@ NTSTATUS
 NTAPI
 HalpBuildAcpiResourceList(IN PIO_RESOURCE_REQUIREMENTS_LIST ResourceList)
 {
+    PIO_RESOURCE_DESCRIPTOR Descriptor;
     ULONG Interrupt;
+    ULONG Vector;
     PAGED_CODE();
     ASSERT(ResourceList != NULL);
 
@@ -1043,6 +1072,28 @@ HalpBuildAcpiResourceList(IN PIO_RESOURCE_REQUIREMENTS_LIST ResourceList)
     ResourceList->List[0].Version = 1;
     ResourceList->List[0].Revision = 1;
     ResourceList->List[0].Count = 0;
+
+    /* The APIC HALs hand the ACPI driver a block of IDT entries it assigns to devices */
+    if (HalpInterruptControllerType == HALP_INTERRUPT_CONTROLLER_APIC)
+    {
+        Descriptor = ResourceList->List[0].Descriptors;
+        for (Vector = HALP_ACPI_DEVICE_VECTOR_FIRST;
+             Vector <= HALP_ACPI_DEVICE_VECTOR_LAST;
+             Vector++)
+        {
+            if (!HalpIsAcpiDeviceVectorFree(Vector))
+                continue;
+
+            Descriptor->Type = CmResourceTypeInterrupt;
+            Descriptor->ShareDisposition = CmResourceShareDeviceExclusive;
+            Descriptor->u.Interrupt.MinimumVector = Vector;
+            Descriptor->u.Interrupt.MaximumVector = Vector;
+            Descriptor++;
+            ResourceList->List[0].Count++;
+        }
+
+        return STATUS_SUCCESS;
+    }
 
     /* Is there a SCI? */
     if (HalpFixedAcpiDescTable.sci_int_vector)
