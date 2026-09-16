5eb164bc85003a2331c11dbfb266b5f7d3516db8
Justin Miller <justin.miller@reactos.org>
2026-09-15 18:28:01 -0700

    [HALX86] Add the legacy PC interrupt translators

--- hal/halx86/include/halacpi.h
+++ hal/halx86/include/halacpi.h
@@ -66,18 +66,6 @@ HalpPublishMmConfigRanges(
     VOID
 );
 
-/* irqtrans.c */
-NTSTATUS
-NTAPI
-HaliGetInterruptTranslator(
-    _In_ INTERFACE_TYPE ParentInterfaceType,
-    _In_ ULONG ParentBusNumber,
-    _In_ INTERFACE_TYPE BridgeInterfaceType,
-    _In_ USHORT Size,
-    _In_ USHORT Version,
-    _Out_ PTRANSLATOR_INTERFACE Translator,
-    _Out_ PULONG BridgeBusNumber);
-
 CODE_SEG("INIT")
 NTSTATUS
 NTAPI
--- hal/halx86/include/halp.h
+++ hal/halx86/include/halp.h
@@ -544,6 +544,18 @@ HaliInitPnpDriver(
     VOID
 );
 
+/* acpi/irqtrans.c, legacy/irqtrans.c */
+NTSTATUS
+NTAPI
+HaliGetInterruptTranslator(
+    _In_ INTERFACE_TYPE ParentInterfaceType,
+    _In_ ULONG ParentBusNumber,
+    _In_ INTERFACE_TYPE BridgeInterfaceType,
+    _In_ USHORT Size,
+    _In_ USHORT Version,
+    _Out_ PTRANSLATOR_INTERFACE Translator,
+    _Out_ PULONG BridgeBusNumber);
+
 CODE_SEG("INIT")
 VOID
 NTAPI
