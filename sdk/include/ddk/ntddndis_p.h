c4eb8da88dea388b1a837f3cbee06dcefa7181bc
Justin Miller <justin.miller@reactos.org>
2026-09-20 21:54:04 -0700

    [NETCX] Add the private NDIS and adapter headers and the RSC library stub

---
+++ sdk/include/ddk/ntddndis_p.h
@@ -0,0 +1,29 @@
+/*
+ * PROJECT:     ReactOS NDIS 6 support
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Private view of the NDIS device interface
+ */
+
+#pragma once
+
+#include <ntddndis.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * How far a selectively suspended adapter is allowed to go idle. The class
+ * extension maps it onto the wake sources the client driver armed.
+ */
+typedef enum _NDIS_IDLE_CONDITION
+{
+    NdisIdleConditionAnyLowLatency = 0,
+    NdisIdleConditionAny,
+    NdisIdleConditionUnicastOnly,
+    NdisIdleConditionL2ConnectedOnly
+} NDIS_IDLE_CONDITION, *PNDIS_IDLE_CONDITION;
+
+#ifdef __cplusplus
+}
+#endif
