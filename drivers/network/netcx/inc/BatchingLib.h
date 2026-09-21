20879a42e5b80847be1593c4f8e03fce1818da14
Justin Miller <justin.miller@reactos.org>
2026-09-20 20:41:25 -0700

    [NETCX] Stub the software segmentation and checksum library

---
+++ drivers/network/netcx/inc/BatchingLib.h
@@ -0,0 +1,51 @@
+/*
+ * PROJECT:     ReactOS NetAdapterCx
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Batched buffer operation library
+ *
+ * Microsoft links this from an internal source tree. It is in no WDK and not
+ * in the shipping netadaptercx.sys either, so only the shape the imported
+ * sources need is declared here. The implementation in
+ * translator/seglibstub.cpp is a stub that announces itself, see there.
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct _BATCHING_LIB_CONTEXT
+{
+    PVOID Reserved;
+} BATCHING_LIB_CONTEXT;
+
+typedef struct _BATCHING_BUFFER_CONTEXT
+{
+    BATCHING_LIB_CONTEXT *Context;
+    PVOID Reserved;
+} BATCHING_BUFFER_CONTEXT;
+
+VOID
+BLInitialize(
+    _Inout_ BATCHING_LIB_CONTEXT *Context);
+
+VOID
+BLUninitialize(
+    _Inout_ BATCHING_LIB_CONTEXT *Context);
+
+VOID
+BLInitializeBatchOpContext(
+    _In_ BATCHING_LIB_CONTEXT *Context,
+    _Out_ BATCHING_BUFFER_CONTEXT *BufferContext,
+    _In_ BOOLEAN Reserved1,
+    _In_ BOOLEAN Reserved2,
+    _In_opt_ PVOID Reserved3);
+
+VOID
+BLFlushBatchOpContext(
+    _Inout_ BATCHING_BUFFER_CONTEXT *BufferContext);
+
+#ifdef __cplusplus
+}
+#endif
