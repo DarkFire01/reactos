2c5f0c3b66f87e5f8b5fae004f91caa1107ee9c9
Justin Miller <justin.miller@reactos.org>
2026-09-20 21:18:37 -0700

    [NETCX] Add the private half of the ring extension headers

---
+++ sdk/include/ddk/net/checksumtypes_p.h
@@ -0,0 +1,11 @@
+/*
+ * PROJECT:     ReactOS NetAdapterCx support
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Private view of the checksum offload extension types
+ */
+
+#pragma once
+
+#include <net/checksumtypes.h>
+
+#define NET_PACKET_EXTENSION_CHECKSUM_VERSION_1_SIZE  sizeof(NET_PACKET_CHECKSUM)
---
+++ sdk/include/ddk/net/returncontexttypes_p.h
@@ -0,0 +1,11 @@
+/*
+ * PROJECT:     ReactOS NetAdapterCx support
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Private view of the return context fragment extension types
+ */
+
+#pragma once
+
+#include <net/returncontexttypes.h>
+
+#define NET_FRAGMENT_EXTENSION_RETURN_CONTEXT_VERSION_1_SIZE  sizeof(NET_FRAGMENT_RETURN_CONTEXT)
