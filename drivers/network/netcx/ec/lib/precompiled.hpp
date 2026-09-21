c78eeab4be023375c5db99f7045d42a6e871f366
Justin Miller <justin.miller@reactos.org>
2026-09-20 19:29:29 -0700

    [NETCX] Add the missing precompiled headers

---
+++ drivers/network/netcx/ec/lib/precompiled.hpp
@@ -0,0 +1,9 @@
+// Copyright (C) Microsoft Corporation. All rights reserved.
+#pragma once
+
+#include <ntddk.h>
+#include <wdf.h>
+#include <ntassert.h>
+#include <KMacros.h>
+#include <NxTrace.hpp>
+#include <NxTraceLogging.hpp>
