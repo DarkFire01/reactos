3d20706b2f24754279f4e448624d3100fff4349b
Justin Miller <justin.miller@reactos.org>
2026-09-20 21:05:55 -0700

    [NETCX] Add the client driver umbrella header

---
+++ sdk/include/ddk/netcx/netadaptercx.h
@@ -0,0 +1,28 @@
+/*
+ * PROJECT:     ReactOS NetAdapterCx support
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     NetAdapterCx client driver interface
+ */
+
+#pragma once
+
+#include <ndis.h>
+
+#include <net/extension.h>
+#include <net/fragment.h>
+#include <net/packet.h>
+#include <net/ring.h>
+#include <net/ringcollection.h>
+
+#include <netcx/netadaptercxtypes.h>
+#include <netcx/netfuncenum.h>
+#include <netcx/netdevice.h>
+#include <netcx/netadapterpacket.h>
+#include <netcx/nettxqueue.h>
+#include <netcx/netrxqueue.h>
+#include <netcx/netadapter.h>
+#include <netcx/netpoweroffload.h>
+#include <netcx/netpoweroffloadlist.h>
+#include <netcx/netwakesource.h>
+#include <netcx/netwakesourcelist.h>
+#include <netcx/netconfiguration.h>
