43c67398dab3bccd09d7bde0fd580c589c0830a3
Justin Miller <justin.miller@reactos.org>
2026-09-20 23:54:03 -0700

    [NETCX] Add CFxObject and stop picking up the KMDF FxObject header

---
+++ drivers/network/netcx/inc/FxObject.hpp
@@ -0,0 +1,9 @@
+/*
+ * PROJECT:     ReactOS NetAdapterCx
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     WDF object wrappers for the queue objects
+ */
+
+#pragma once
+
+#include <FxObjectBase.hpp>
