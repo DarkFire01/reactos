0a074f4f88e87e79f2250060854ea06ec4ad75e1
Justin Miller <justin.miller@reactos.org>
2026-09-22 08:17:43 -0700

    [NTOS:RTL][NDK][SDK] Implement RtlAvlInsertNodeEx and RtlAvlRemoveNode

--- sdk/include/ndk/rtltypes.h
+++ sdk/include/ndk/rtltypes.h
@@ -980,6 +980,16 @@ typedef struct _ACTIVATION_CONTEXT_STACK_FRAMELIST
 
 #endif /* NTOS_MODE_USER */
 
+//
+// AVL tree of RTL_BALANCED_NODE entries, the balance lives in ParentValue
+//
+#ifndef NTOS_MODE_USER
+typedef struct _RTL_AVL_TREE
+{
+    PRTL_BALANCED_NODE Root;
+} RTL_AVL_TREE, *PRTL_AVL_TREE;
+#endif /* !NTOS_MODE_USER */
+
 #if (NTDDI_VERSION >= NTDDI_WS03SP1)
 typedef struct _ACTIVATION_CONTEXT_STACK
 {
