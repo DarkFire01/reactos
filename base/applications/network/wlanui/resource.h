27ea6fa61a87de9e57cb90c32ba0379267d9a937
Justin Miller <justin.miller@reactos.org>
2026-09-22 01:17:37 -0700

    wlanui: a small scan and connect front end

---
+++ base/applications/network/wlanui/resource.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define IDC_STATIC          -1
+
+#define IDD_MAIN            100
+#define IDI_WLANUI          101
+
+#define IDC_NETWORKS        1001
+#define IDC_KEY             1002
+#define IDC_SCAN            1003
+#define IDC_CONNECT         1004
+#define IDC_STATUS          1005
+
+#define IDS_APP_TITLE       200
