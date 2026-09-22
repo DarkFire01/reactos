c1a15c0f53cb5d7c8a59937d62a3b886abcc044a
Justin Miller <justin.miller@reactos.org>
2026-09-22 08:17:07 -0700

    [NTOS:EX] Implement ExInitializeLookasideListEx, ExFlushLookasideListEx and ExDeleteLookasideListEx

--- ntoskrnl/ex/lookas.c
+++ ntoskrnl/ex/lookas.c
@@ -314,4 +314,194 @@ ExInitializePagedLookasideList(IN PPAGED_LOOKASIDE_LIST Lookaside,
                                 &ExpPagedLookasideListLock);
 }
 
+/* Pool bits a LOOKASIDE_LIST_EX may carry; raise and quota flags go through Flags */
+#define EXP_LOOKASIDE_EX_POOL_BITS 0x3E7
+
+/* Bit 0 of a pool type tells paged pool from nonpaged pool */
+#define EXP_IS_PAGED_POOL(Type) (((Type) & 1) == PagedPool)
+
+static
+PVOID
+NTAPI
+ExpAllocateLookasideEntryEx(
+    _In_ POOL_TYPE PoolType,
+    _In_ SIZE_T NumberOfBytes,
+    _In_ ULONG Tag,
+    _Inout_ PLOOKASIDE_LIST_EX Lookaside)
+{
+    UNREFERENCED_PARAMETER(Lookaside);
+
+    if (PoolType & POOL_QUOTA_FAIL_INSTEAD_OF_RAISE)
+        return ExAllocatePoolWithQuotaTag(PoolType, NumberOfBytes, Tag);
+
+    return ExAllocatePoolWithTag(PoolType, NumberOfBytes, Tag);
+}
+
+static
+VOID
+NTAPI
+ExpFreeLookasideEntryEx(
+    _In_ PVOID Buffer,
+    _Inout_ PLOOKASIDE_LIST_EX Lookaside)
+{
+    UNREFERENCED_PARAMETER(Lookaside);
+    ExFreePool(Buffer);
+}
+
+/**
+ * @brief
+ * Initializes a lookaside list that may use any pool type.
+ *
+ * @param[out] Lookaside
+ * The list to initialize.
+ *
+ * @param[in] Allocate
+ * Optional allocation routine. The pool is used when NULL.
+ *
+ * @param[in] Free
+ * Optional free routine. The pool is used when NULL.
+ *
+ * @param[in] PoolType
+ * Pool the entries come from.
+ *
+ * @param[in] Flags
+ * EX_LOOKASIDE_LIST_EX_FLAGS_RAISE_ON_FAIL or
+ * EX_LOOKASIDE_LIST_EX_FLAGS_FAIL_NO_RAISE.
+ *
+ * @param[in] Size
+ * Size of each entry.
+ *
+ * @param[in] Tag
+ * Pool tag of the entries.
+ *
+ * @param[in] Depth
+ * Zero for the default depth, otherwise 256 to 1024.
+ *
+ * @return
+ * STATUS_SUCCESS, or STATUS_INVALID_PARAMETER_4, _5 or _8 for a bad pool
+ * type, flag or depth.
+ */
+NTSTATUS
+NTAPI
+ExInitializeLookasideListEx(
+    _Out_ PLOOKASIDE_LIST_EX Lookaside,
+    _In_opt_ PALLOCATE_FUNCTION_EX Allocate,
+    _In_opt_ PFREE_FUNCTION_EX Free,
+    _In_ POOL_TYPE PoolType,
+    _In_ ULONG Flags,
+    _In_ SIZE_T Size,
+    _In_ ULONG Tag,
+    _In_ USHORT Depth)
+{
+    PGENERAL_LOOKASIDE_POOL List = &Lookaside->L;
+    ULONG PoolFlags = 0;
+
+    if (Depth == 0)
+    {
+        Depth = EX_MAXIMUM_LOOKASIDE_DEPTH_BASE;
+    }
+    else if ((Depth < EX_MAXIMUM_LOOKASIDE_DEPTH_BASE) ||
+             (Depth > EX_MAXIMUM_LOOKASIDE_DEPTH_LIMIT))
+    {
+        return STATUS_INVALID_PARAMETER_8;
+    }
+
+    if (Flags == EX_LOOKASIDE_LIST_EX_FLAGS_RAISE_ON_FAIL)
+        PoolFlags = POOL_RAISE_IF_ALLOCATION_FAILURE;
+    else if (Flags == EX_LOOKASIDE_LIST_EX_FLAGS_FAIL_NO_RAISE)
+        PoolFlags = POOL_QUOTA_FAIL_INSTEAD_OF_RAISE;
+    else if (Flags != 0)
+        return STATUS_INVALID_PARAMETER_5;
+
+    if ((PoolType & ~EXP_LOOKASIDE_EX_POOL_BITS) ||
+        ((PoolType & 3) == 3))
+    {
+        return STATUS_INVALID_PARAMETER_4;
+    }
+
+    if (Size < LOOKASIDE_MINIMUM_BLOCK_SIZE)
+        Size = LOOKASIDE_MINIMUM_BLOCK_SIZE;
+
+    InitializeSListHead(&List->ListHead);
+    List->Depth = 4;
+    List->MaximumDepth = Depth;
+    List->TotalAllocates = 0;
+    List->AllocateMisses = 0;
+    List->TotalFrees = 0;
+    List->FreeMisses = 0;
+    List->Type = PoolType | PoolFlags;
+    List->Tag = Tag;
+    List->Size = (ULONG)Size;
+    List->AllocateEx = Allocate ? Allocate : ExpAllocateLookasideEntryEx;
+    List->FreeEx = Free ? Free : ExpFreeLookasideEntryEx;
+    List->LastTotalAllocates = 0;
+    List->LastAllocateMisses = 0;
+
+    if (EXP_IS_PAGED_POOL(PoolType))
+    {
+        ExInterlockedInsertTailList(&ExpPagedLookasideListHead,
+                                    &List->ListEntry,
+                                    &ExpPagedLookasideListLock);
+    }
+    else
+    {
+        ExInterlockedInsertTailList(&ExpNonPagedLookasideListHead,
+                                    &List->ListEntry,
+                                    &ExpNonPagedLookasideListLock);
+    }
+
+    return STATUS_SUCCESS;
+}
+
+/**
+ * @brief
+ * Frees every entry held by a lookaside list.
+ *
+ * @param[in,out] Lookaside
+ * The list to flush.
+ */
+VOID
+NTAPI
+ExFlushLookasideListEx(
+    _Inout_ PLOOKASIDE_LIST_EX Lookaside)
+{
+    PSLIST_ENTRY Entry;
+    PSLIST_ENTRY Next;
+
+    for (Entry = InterlockedFlushSList(&Lookaside->L.ListHead); Entry; Entry = Next)
+    {
+        Next = Entry->Next;
+        Lookaside->L.FreeEx(Entry, Lookaside);
+    }
+}
+
+/**
+ * @brief
+ * Removes a lookaside list from the system and frees its entries.
+ *
+ * @param[in,out] Lookaside
+ * The list to delete.
+ */
+VOID
+NTAPI
+ExDeleteLookasideListEx(
+    _Inout_ PLOOKASIDE_LIST_EX Lookaside)
+{
+    PKSPIN_LOCK ListLock;
+    KIRQL OldIrql;
+
+    if (EXP_IS_PAGED_POOL(Lookaside->L.Type))
+        ListLock = &ExpPagedLookasideListLock;
+    else
+        ListLock = &ExpNonPagedLookasideListLock;
+
+    KeAcquireSpinLock(ListLock, &OldIrql);
+    RemoveEntryList(&Lookaside->L.ListEntry);
+    KeReleaseSpinLock(ListLock, OldIrql);
+
+    /* Frees from now on go straight back to the pool */
+    Lookaside->L.Depth = 0;
+    ExFlushLookasideListEx(Lookaside);
+}
+
 /* EOF */
